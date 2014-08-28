3d255493c5202b8670badeead457523120b0f4d1
Twinaphex <libretro@gmail.com>
2014-08-28 05:59:39 +0200

    (iOS) Some build fixes for iOS - was previously hardcoded for IOS 6.1 SDK

--- ios-build.sh
+++ ios-build.sh
@@ -5,7 +5,7 @@ PLATFORM=/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/
 GENERAL="\
    --enable-cross-compile \
    --arch=arm \
-   --cc=$PLATFORM/usr/bin/gcc"
+   --cc=clang"
 
 MODULES="\
    --disable-filters \
@@ -69,11 +69,11 @@ MUXERS="\
 ./configure \
     --prefix=ios/armv7 \
     $GENERAL \
-    --sysroot="$PLATFORM/SDKs/iPhoneOS6.1.sdk" \
-    --extra-cflags="-arch armv7 -mfpu=neon -miphoneos-version-min=6.0" \
+    --sysroot="$(xcrun -sdk iphoneos -show-sdk-path)" \
+    --extra-cflags="-arch armv7 -mfpu=neon -miphoneos-version-min=5.0" \
     --disable-shared \
     --enable-static \
-    --extra-ldflags="-arch armv7 -isysroot $PLATFORM/SDKs/iPhoneOS6.1.sdk -miphoneos-version-min=6.0" \
+    --extra-ldflags="-arch armv7 -isysroot $(xcrun -sdk iphoneos -show-sdk-path) -miphoneos-version-min=5.0" \
     --enable-zlib \
     --enable-pic \
     --disable-everything \
@@ -83,7 +83,7 @@ MUXERS="\
     ${VIDEO_ENCODERS} \
     ${AUDIO_ENCODERS} \
     ${DEMUXERS} \
-		${MUXERS} \
+    ${MUXERS} \
     ${PARSERS} \
     --target-os=darwin \
     --enable-vfp \
@@ -101,11 +101,11 @@ fi
 ./configure \
     --prefix=ios/armv7s \
     $GENERAL \
-    --sysroot="$PLATFORM/SDKs/iPhoneOS6.1.sdk" \
-    --extra-cflags="-arch armv7s -mfpu=neon -miphoneos-version-min=6.0" \
+    --sysroot="$(xcrun -sdk iphoneos -show-sdk-path)" \
+    --extra-cflags="-arch armv7s -mfpu=neon -miphoneos-version-min=5.0" \
     --disable-shared \
     --enable-static \
-    --extra-ldflags="-arch armv7s -isysroot $PLATFORM/SDKs/iPhoneOS6.1.sdk -miphoneos-version-min=6.0" \
+    --extra-ldflags="-arch armv7s -isysroot $(xcrun -sdk iphoneos -show-sdk-path)" -miphoneos-version-min=5.0" \
     --enable-zlib \
     --enable-pic \
     --disable-everything \
